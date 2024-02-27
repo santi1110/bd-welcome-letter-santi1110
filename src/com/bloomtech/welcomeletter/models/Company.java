fc4db088a2a5ef95bf888f5cf3e8c3ecbbba5ac8
Santiago Coria <santiago_coria@hotmail.com>
2024-02-27 12:37:42 -0300

    Mastery Task1

--- src/com/bloomtech/welcomeletter/models/Company.java
+++ src/com/bloomtech/welcomeletter/models/Company.java
@@ -7,8 +7,19 @@ public class Company {
     private String companyname;
     private String country;
 
+
+    public static Builder builder(){
+        return new Builder();
+    }
+
+    private Company(Builder builder){
+        this.id = builder.id;
+        this.companyname = builder.companyname;
+        this.country = builder.country;
+    }
+
     //TODO: Replace with Builder Constructor
-    public Company(String companyname, String country) {
+/*    public Company(String companyname, String country) {
         //Auto-generate an id
         id = maxId;
         maxId++;
@@ -16,14 +27,43 @@ public class Company {
         //Initialize fields
         this.companyname = companyname;
         this.country = country;
-    }
+    }*/
 
     public static final class Builder {
+        private static int maxId = 1;
+        private int id;
+
+        private String companyname;
+        private String country;
+
+        public Builder(){
+            id  = maxId;
+            maxId++;
+        }
+
+
+
         //TODO: Implement Builder Pattern
+    public Builder withId(int idToUse){
+            this.id = idToUse;
+            return this;
+    }
+    public Builder withCompanyname(String companynameToUse){
+            this.companyname = companynameToUse;
+            return this;
+    }
+    public Builder withCountry(String countryToUse){
+            this.country = countryToUse;
+            return this;
+    }
+    public Company build(){
+            return new Company(this);
     }
 
+
     public int getId() {
-        return id;
+
+            return id;
     }
 
     public String getCompanyname() {
@@ -33,4 +73,4 @@ public class Company {
     public String getCountry() {
         return country;
     }
-}
+}}
--- src/com/bloomtech/welcomeletter/models/Employee.java
+++ src/com/bloomtech/welcomeletter/models/Employee.java
@@ -4,7 +4,6 @@ import java.time.LocalDate;
 
 public class Employee {
     private static int maxId = 1;
-
     private int id;
     private String firstname;
     private String lastname;
@@ -15,10 +14,30 @@ public class Employee {
     private Role role;
     private Company company;
 
+
+
+
+    public static Employeebuilder builder(){
+        return new Employeebuilder();
+    }
+    private Employee(Employeebuilder builder){
+        this.id = builder.id;
+        this.firstname = builder.firstname;
+        this.lastname = builder.lastname;
+        this.phonenumber = builder.phonenumber;
+        this.email = builder.email;
+        this.salary = builder.salary;
+        this.startdate = builder.startdate;
+        this.role = builder.role;
+        this.company = builder.company;
+
+    }
+
     //TODO: Implement Builder Pattern
 
     //TODO: Replace with Builder Constructor
-    public Employee(String firstname, String lastname, String phonenumber, String email, int salary, LocalDate startdate, Role role, Company company) {
+
+/*    public Employee(String firstname, String lastname, String phonenumber, String email, int salary, LocalDate startdate, Role role, Company company) {
         //Auto-generate an id
         id = maxId;
         maxId++;
@@ -32,10 +51,70 @@ public class Employee {
         this.startdate = startdate;
         this.role = role;
         this.company = company;
-    }
+    }*/
 
     //TODO: Create Builder Class
+public static class Employeebuilder{
+        private  int id;
+        private static int maxId = 1;
+        private String firstname;
+        private String lastname;
+        private String phonenumber;
+        private String email;
+        private int salary;
+        private LocalDate startdate;
+        private Role role;
+        private Company company;
 
+        public Employeebuilder() {
+            id = maxId;
+            maxId++;
+        }
+
+        public Employeebuilder withId(int idToUse){
+            this.id = idToUse;
+            return this;
+        }
+        public Employeebuilder withFirstname(String firstnameToUse){
+            this.firstname = firstnameToUse;
+            return this;
+        }
+        public Employeebuilder withLastname(String lastnameToUse){
+            this.lastname = lastnameToUse;
+            return this;
+        }
+        public Employeebuilder withPhonenumber(String phonenumberToUse){
+            this.phonenumber = phonenumberToUse;
+            return this;
+        }
+        public Employeebuilder withEmail(String emailToUse){
+            this.email = emailToUse;
+            return this;
+        }
+        public Employeebuilder withSalary(int salaryToUse){
+            this.salary = salaryToUse;
+            return this;
+        }
+        public Employeebuilder withStartdate(LocalDate startdateToUse){
+            this.startdate = startdateToUse;
+            return this;
+        }
+        public Employeebuilder withRole(Role roleToUse){
+            this.role = roleToUse;
+            return this;
+        }
+        public Employeebuilder withCompany(Company companyToUse){
+            this.company = companyToUse;
+            return this;
+        }
+
+    public Employee build(){
+        if (firstname == null){
+            throw new RuntimeException("Firstname cannot be null");}
+        if (company == null){
+            throw new RuntimeException("Company cannot be null");}
+        return new Employee(this);
+    }}
 
     //Getters
     public String getFirstname() {
